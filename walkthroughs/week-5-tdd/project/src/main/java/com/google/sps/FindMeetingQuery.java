8b24ae69df64bc97a4eddddeb6df1a60e9146f64
Jordan Grant <you@example.comjordangrant46@gmail.comgit config --global user.name Jordan>
2020-06-19 17:50:40 +0000

    Query Algorithm

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,6 +15,10 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Set;
 
 public final class FindMeetingQuery {
 
@@ -28,14 +32,22 @@ public final class FindMeetingQuery {
 
         // Array of queries represenitng times to meet.
         List<TimeRange> query = new ArrayList<>();
+        
         // Integer represenitng the number of events.
         int numberOfEvents = events.size();
+        
         // Integer representing the Start of a work day.
         int timeOfStart = START_OF_DAY;
+
+        // Integer representing the End of a work day.
+        int timeOfEnd = END_OF_DAY;
+        
         // Integer represenig the duration of a meeting request.
         int desiredDuration = (int) request.getDuration();
-
-        int counter = 0;
+        
+        //  Event counter used whilst iterating.
+        int eventCounter = 0;
+        
         int event_start = 0;
         int event_end = 0;
 
@@ -44,7 +56,9 @@ public final class FindMeetingQuery {
 
         // Collection of string reprenting who is to attend the requedted meeting
         Collection<String> request_attendees = request.getAttendees();
-        boolean ignore_event = true;
+        
+        // Boolean used to indictae whter or not the attendee should be ignored.
+        boolean shouldIgnore = true;
 
         // Check if duration of event is longer than a whole day and if so return empty.
         if ((int)request.getDuration() > TimeRange.WHOLE_DAY.duration()) {
@@ -52,52 +66,61 @@ public final class FindMeetingQuery {
         }
 
         // Check if there are any events happening at all if so return whole day query.
-        if (num_events == 0) {
+        if (numberOfEvents == 0) {
+            // Adds a 24 hour time range to query.
             query.add(TimeRange.WHOLE_DAY);
             return query;
         }
+        // Iterating throguh input event collection. 
+        for (Event event : events) {
 
-        for (Event e : events) {
-
-            event_attendees = e.getAttendees();
+            // Collection of the pople who are to attend meeting.
+            attendees = event.getAttendees();
 
             //Ignore event only if attendees of this event are not needed in requested meeting
-            for (String a : event_attendees) {
-                if (request_attendees.contains(a)) {
-                    ignore_event = false;
+            for (String attendee : attendees) {
+                // If this attendee is required
+                if (request_attendees.contains(attendee)) {
+                    // You shouldn't ignore this attendee. 
+                    shouldIgnore = false;
                     break;
-                } else if (counter == num_events-1) {
-                    query.add(TimeRange.fromStartEnd(next_available_start, TimeRange.END_OF_DAY, true));
+                } 
+                // If at the last event.
+                else if (eventCounter == numberOfEvents-1) {
+                    query.add(TimeRange.fromStartEnd(timeOfStart, timeOfEnd, true));
                 }
             }
+            // if the attendee is mandatory.
+            if (!shouldIgnore) {
+                
+                // Fetch event start time.
+                event_start = event.getWhen().start();
+                // Fetch event end time.
+                event_end = event.getWhen().end();
 
-            if (!ignore_event) {
-                event_start = e.getWhen().start();
-                event_end = e.getWhen().end();
-
-                if (event_start == TimeRange.START_OF_DAY) {
-                    next_available_start = event_end;
+                if (event_start == timeOfStart) {
+                    timeOfStart = event_end;
                 }
 
                 // Check for overlapping/nested events
-                if (event_start < next_available_start) {
-                    if (event_end > next_available_start) {
-                        next_available_start = event_end;
+                if (event_start < timeOfStart) {
+                    if (event_end > timeOfStart) {
+                        timeOfStart = event_end;
                     }
                 }
 
                 //Non-overlapping/non-nested events
-                if (next_available_start <= event_start) {
-                    if (event_start - next_available_start >= desired_duration) {
-                        query.add(TimeRange.fromStartEnd(next_available_start, event_start, false));
+                if (timeOfStart <= event_start) {
+                    if (event_start - timeOfStart >= desiredDuration) {
+                        query.add(TimeRange.fromStartEnd(timeOfStart, event_start, false));
                     }
-                    next_available_start = event_end;
+                    timeOfStart = event_end;
                 }
 
                 // Last event of the day
-                if (counter == num_events-1) {
-                    if (next_available_start != TimeRange.END_OF_DAY+1) {
-                        query.add(TimeRange.fromStartEnd(next_available_start, TimeRange.END_OF_DAY, true));
+                if (eventCounter == numberOfEvents-1) {
+                    if (timeOfStart != timeOfEnd + 1) {
+                        query.add(TimeRange.fromStartEnd(timeOfStart, TimeRange.END_OF_DAY, true));
                     }
                 }
             }
