88de33795386f1152a51293c9ab04d216ae01b44
Jordan Grant <you@example.comjordangrant46@gmail.comgit config --global user.name Jordan>
2020-06-23 14:23:59 +0000

    Fixing Errors

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,54 +14,47 @@
 
 package com.google.sps;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
-import java.util.ArrayList;
 import java.util.Set;
 
 public final class FindMeetingQuery {
 
-    public static final int START_OF_DAY = TimeRange.START_OF_DAY;
-    public static final int END_OF_DAY = TimeRange.END_OF_DAY;
-    public static final TimeRange WHOLE_DAY = TimeRange.WHOLE_DAY;
-    public static final Comparator<TimeRange> ORDER_BY_START = TimeRange.ORDER_BY_START;
-
+    private static final int START_OF_DAY = TimeRange.START_OF_DAY;
+    private static final int END_OF_DAY = TimeRange.END_OF_DAY;
+    private static final TimeRange WHOLE_DAY = TimeRange.WHOLE_DAY;
+    private static final Comparator<TimeRange> ORDER_BY_START = TimeRange.ORDER_BY_START;
 
     public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
 
         // Array of queries represenitng times to meet.
         List<TimeRange> query = new ArrayList<>();
         
-        // Integer represenitng the number of events.
         int numberOfEvents = events.size();
         
-        // Integer representing the Start of a work day.
         int timeOfStart = START_OF_DAY;
-
-        // Integer representing the End of a work day.
-        int timeOfEnd = END_OF_DAY;
         
-        // Integer represenig the duration of a meeting request.
         int desiredDuration = (int) request.getDuration();
         
         //  Event counter used whilst iterating.
         int eventCounter = 0;
         
-        int event_start = 0;
-        int event_end = 0;
+        int eventStart = 0;
+        int eventEnd = 0;
 
         // Set of stirngs representing unique employees set to attend this meeting.
-        Set<String> event_attendees = null;
+        Set<String> attendees = new Set();
 
         // Collection of string reprenting who is to attend the requedted meeting
-        Collection<String> request_attendees = request.getAttendees();
+        Collection<String> requestAttendees = request.getAttendees();
         
         // Boolean used to indictae whter or not the attendee should be ignored.
         boolean shouldIgnore = true;
 
         // Check if duration of event is longer than a whole day and if so return empty.
-        if ((int)request.getDuration() > TimeRange.WHOLE_DAY.duration()) {
+        if ((int)request.getDuration() > WHOLE_DAY.duration()) {
             return query;
         }
 
@@ -72,7 +65,7 @@ public final class FindMeetingQuery {
             return query;
         }
         // Iterating throguh input event collection. 
-        for (Event event : events) {
+        for (Event event : events.sort(event.ORDER_BY_START)) {
 
             // Collection of the pople who are to attend meeting.
             attendees = event.getAttendees();
@@ -80,55 +73,60 @@ public final class FindMeetingQuery {
             //Ignore event only if attendees of this event are not needed in requested meeting
             for (String attendee : attendees) {
                 // If this attendee is required
-                if (request_attendees.contains(attendee)) {
+                if (requestAttendees.contains(attendee)) {
                     // You shouldn't ignore this attendee. 
                     shouldIgnore = false;
                     break;
                 } 
                 // If at the last event.
-                else if (eventCounter == numberOfEvents-1) {
-                    query.add(TimeRange.fromStartEnd(timeOfStart, timeOfEnd, true));
+                if (eventCounter == numberOfEvents-1) {
+                    // Add time range of current start time to end of day.
+                    query.add(TimeRange.fromStartEnd(timeOfStart, END_OF_DAY, true));
                 }
             }
             // if the attendee is mandatory.
             if (!shouldIgnore) {
                 
                 // Fetch event start time.
-                event_start = event.getWhen().start();
+                eventStart = event.getWhen().start();
                 // Fetch event end time.
-                event_end = event.getWhen().end();
+                eventEnd = event.getWhen().end();
 
-                if (event_start == timeOfStart) {
-                    timeOfStart = event_end;
+
+            //  If the start of the evnt is directly after another or at the start of the day.
+            //  set start of the day to the end of the meeting.
+                if (eventStart == timeOfStart) {
+                    timeOfStart = eventEnd;
                 }
 
                 // Check for overlapping/nested events
-                if (event_start < timeOfStart) {
-                    if (event_end > timeOfStart) {
-                        timeOfStart = event_end;
+                if (eventStart < timeOfStart) {
+                    if (eventEnd > timeOfStart) {
+                        timeOfStart = eventEnd;
                     }
                 }
 
                 //Non-overlapping/non-nested events
-                if (timeOfStart <= event_start) {
-                    if (event_start - timeOfStart >= desiredDuration) {
-                        query.add(TimeRange.fromStartEnd(timeOfStart, event_start, false));
+                if (timeOfStart <= eventStart) {
+                    if (eventStart - timeOfStart >= desiredDuration) {
+                        query.add(TimeRange.fromStartEnd(timeOfStart, eventStart, false));
                     }
-                    timeOfStart = event_end;
+                    timeOfStart = eventEnd;
                 }
 
-                // Last event of the day
-                if (eventCounter == numberOfEvents-1) {
-                    if (timeOfStart != timeOfEnd + 1) {
+                // If at the last event and 
+                if (eventCounter == numberOfEvents - 1) {
+                    if (timeOfStart != END_OF_DAY + 1) {
                         query.add(TimeRange.fromStartEnd(timeOfStart, TimeRange.END_OF_DAY, true));
                     }
                 }
             }
-
-            counter += 1;
-            ignore_event = true;
+            //  Increment event idx by one/
+            eventCounter += 1;
+            //  Else set the ignore case to true
+            shouldIgnore = true;
         }
-
+        //  Return the meetings.
         return query;
 
     }
