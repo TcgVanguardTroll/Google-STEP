dea469cc229657c71af0dc272c978a9b73bc2c77
Jordan Grant <jordangrant46@gmail.com>
2020-08-04 17:51:56 +0000

    format

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -12,20 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Copyright 2019 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package com.google.sps;
 
 import java.util.ArrayList;
@@ -42,8 +28,7 @@ public final class FindMeetingQuery {
   private static final TimeRange WHOLE_DAY = TimeRange.WHOLE_DAY;
   private static final Comparator<TimeRange> ORDER_BY_START = TimeRange.ORDER_BY_START;
 
-  public Collection<TimeRange> query(
-      Collection<Event> events, MeetingRequest request) {
+  public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
 
     int desiredDuration = (int) request.getDuration();
 
@@ -60,32 +45,35 @@ public final class FindMeetingQuery {
 
     // If there are no mandatory attendees , return lists of the optional attendees time ranges.
     if (mandatoryAttendees.isEmpty()) {
-        List<TimeRange> optionalTimeRanges = populateTimeRanges(events, optionalAttendees, desiredDuration);
-        return optionalTimeRanges;   
+      List<TimeRange> optionalTimeRanges =
+          populateTimeRanges(events, optionalAttendees, desiredDuration);
+      return optionalTimeRanges;
     }
 
     // If there are no optional attendees , return lists of the mandatory attendees time ranges.
     if (optionalAttendees.isEmpty()) {
-      List<TimeRange> mandatoryTimeRanges = populateTimeRanges(events, mandatoryAttendees, desiredDuration);
-      return mandatoryTimeRanges;   
-       }
+      List<TimeRange> mandatoryTimeRanges =
+          populateTimeRanges(events, mandatoryAttendees, desiredDuration);
+      return mandatoryTimeRanges;
+    }
 
     // Collection of Strings representing all attendees ( Mandatory and Optional )
     Collection<String> allAttendees =
         combineMandatoryAndOptional(mandatoryAttendees, optionalAttendees);
 
     // List of queries representing times to meet.
-    List<TimeRange> allTimeRanges;
 
-    allTimeRanges = findAvailableTimeRangesForAttendees(events, allAttendees, desiredDuration);
+    List<TimeRange> allTimeRanges =
+        findAvailableTimeRangesForAttendees(events, allAttendees, desiredDuration);
     if (allTimeRanges.isEmpty()) {
-      List<TimeRange> mandatoryTimeRanges = populateTimeRanges(events, mandatoryAttendees, desiredDuration);
+      List<TimeRange> mandatoryTimeRanges =
+          populateTimeRanges(events, mandatoryAttendees, desiredDuration);
       return mandatoryTimeRanges;
     }
     return allTimeRanges;
   }
 
- public List<TimeRange> findAvailableTimeRangesForAttendees(
+  public List<TimeRange> findAvailableTimeRangesForAttendees(
       Collection<Event> events, Collection<String> attendees, int desiredDuration) {
 
     // Array of queries representing times to meet.
@@ -125,9 +113,7 @@ public final class FindMeetingQuery {
       // If this is a valid time range then add it to query .
       if (currentTime <= eventStart) {
         if (eventStart - currentTime >= desiredDuration) {
-          timeRanges.add(
-              TimeRange.fromStartEnd(
-                  currentTime, eventStart, /* inclusive= */ false));
+          timeRanges.add(TimeRange.fromStartEnd(currentTime, eventStart, /* inclusive= */ false));
         }
         nextTime = eventEnd;
       }
